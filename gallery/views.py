7e749ccccf6eaea377a0ae42edd1bf9e61645353
Mixer2048 <mixerchannel4@gmail.com>
2026-02-13 06:23:05 +0700

    added about

---
+++ gallery/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Привет, это 3D хранилище<h1><p>Система работает.</p>")
+
+def about(request):
+    return HttpResponse("Курс Web структуры")
